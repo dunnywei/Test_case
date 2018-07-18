52d79ecca68346a461342463ddae80e41e789e8e
dunnywei <wei_don@yahoo.com>
2018-07-17 20:55:40 -0400

    Lecture 3 update

---
+++ Lecture3.c
@@ -0,0 +1,35 @@
+->https://www.youtube.com/playlist?list=PLDC2A0C8D2EC934C7
+
+LEC III
+>develop a software (0:19) (SDLC)
+1)Gather as much info as possible about the details and specifications of the desired software from the client (Requirement)
+
+2)Plan and preparing the lanquage like java, php, .net and Database which will be suitable  for the project  with related function (Design)
+
+3)actually code the software (build stage) (1:04)
+
+4)Test the software to verify that it is built as per the specification given by the client (test) (1:122)
+
+5)Maintenance stage for code changed due to  the enhancement requested from client (1:26)
+
+>From 1 to  5 is a.k.a software development life cycle or Waterfall method
+>Wrong product to the client could occur due to mistake in the requirement (1:50)
+
+>V-model of testing (2:45) (SUPER important)
+For each test in SDLC, there is a corresponding for each step of SDLC (2:45)
+*1)Requirement Analysis>User acceptance test plan->User acceptance testing
+*2)Function specification->System test plan->system testing
+*3)High Level Design>Integration Test Plan->Integration test
+*4) Detailed Designed/Program specification->Unit test plan>unit test
+5)code
+
+>LEFT SDLC and right is STLC
+
+>Iterative life cycle (3:33)
+Each phase has a requirement, design, build and test
+-It is either called rapid application development or agile development
+
+>Testing in a life cycle model (3:58)
+-Development model selected for a project depending on the aims and goal of that project
+-Testing is a stand-alone activity and it has to be adapt with the development model chosen for the project
+-In any model, testing should be done in all levels
