1b1d027126206a2900096e607c744f718a21ecf2
dunnywei <wei_don@yahoo.com>
2018-07-17 20:53:53 -0400

    update Lec1 and Lec2

--- Lecture1.c
+++ Lecture1.c
@@ -1,4 +1,36 @@
 /*
 https://www.youtube.com/playlist?list=PLDC2A0C8D2EC934C7
 
+LEC1
+->Software testing is a process used to identify the correctness and quality of developed computer software.
+
+->It include a set of activities conducted with the intent of finding errors in software that it could be  corrected before the product is released to the end user.
+
+->check the software system is defect free
+
+LEC 2
+Moving file from a folder A to folder B
+>Does file exist in A (Me)
+>Do we have the same file in B (Me)
+>What happen if the disk size is full and in a shared drive (Me)
+>To test if the file is open while moving (0:29)
+>security right to pass the file in Folder B
+
+>Exhaust testing is not possible (for 2) (1:12)
+>We need optimal amount of testing based on the RISK assessment of the application (1:21)
+>Muti-tasking module needed to be tested thoroughly
+>Defect Clustering (for 4)(2:03)
+>A small number of modules contain most of the defects detected.
+>With the experience, we can identify such risky modules
+>However, the problem is that the same test are repeated over again. Eventually same test will no longer find new bugs (a.k.a pesticide paradox for 5) (2:18)
+>To overcome this, the test case is required to be reviewed and revised. Adding new and different test cases to help find new defects. (2:28)
+
+>Testing principle stats that->testing shows presence of defects (for 1). For instance, testing reduces the probably of undiscovered defects remaining in the software. But even if no defects are found, it is not a proof of correctness 
+
+>Absence of error is a fallacy (for 7)
+>Next step is: Early testing (for 3) (4:24)
+Testing should start as early as possible in the software development life cycle so any issue is discovered in the design phase as well (4:32)
+
+->Testing is Context dependent (for 6): Test eCommerce such as amazon is different than testing MS office (4:49)
+
 */
